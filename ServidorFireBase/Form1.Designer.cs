d10088c554e55f520568cc1ab4f01c4e702efcea
oswaldo <oswaldo40.jimenez50@gmail.com>
2021-04-23 10:36:26 -0500

    Final

---
+++ ServidorFireBase/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace ServidorFireBase
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEnviarMensaje = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEnviarMensaje
+            // 
+            this.btnEnviarMensaje.Location = new System.Drawing.Point(176, 120);
+            this.btnEnviarMensaje.Name = "btnEnviarMensaje";
+            this.btnEnviarMensaje.Size = new System.Drawing.Size(75, 23);
+            this.btnEnviarMensaje.TabIndex = 0;
+            this.btnEnviarMensaje.Text = "Enviar";
+            this.btnEnviarMensaje.UseVisualStyleBackColor = true;
+            this.btnEnviarMensaje.Click += new System.EventHandler(this.btnEnviarMensaje_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 612);
+            this.Controls.Add(this.btnEnviarMensaje);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEnviarMensaje;
+    }
+}
+
---
+++ ServidorFireBase/Form1.cs
@@ -0,0 +1,62 @@
+﻿using FirebaseAdmin;
+using FirebaseAdmin.Messaging;
+using Google.Apis.Auth.OAuth2;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServidorFireBase
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+
+        private async Task btnPeticion_ClickAsync()
+        {
+            FirebaseApp.Create(new AppOptions()
+            {
+                Credential = GoogleCredential.FromFile("C:/Users/ajent/Documents/proyectofirebase-c8858-firebase-adminsdk-p7zwp-2dec3156a4.json")
+            });
+
+            var registrationToken = "d85ZcfePT8q9sI0-TuUNdK:APA91bE5Gtb3of3t6MRZsBXwNZ8cXwV-vucDqZHnpe74GZzMOyXfBkOs4Ye64GAPqfnOj0Agw9FCo3tIrxldJFi-D-0niouAQ-o_RiqAmFKp_UDo4Twc78eTBpbWbHTxTSCMVxiWopTf";
+
+            var message = new FirebaseAdmin.Messaging.Message()
+            {
+                Notification = new Notification()
+                {
+                    Title = "Este es un Titulo de Notificacion",
+                    Body = "Hola, este es el contenido del mensaje.",
+                },
+                Token = registrationToken,
+            };
+
+           
+            string response = await FirebaseMessaging.DefaultInstance.SendAsync(message);
+
+           
+            MessageBox.Show("Successfully sent message: " + response);
+        }
+
+        private void btnEnviarMensaje_Click(object sender, EventArgs e)
+        {
+            _ = btnPeticion_ClickAsync();
+        }
+    }
+}
